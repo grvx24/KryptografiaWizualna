702d0180fd59ef34b8b105912f9d482fa478c478
Kamil Sagalara <kamil.sagalara@student.put.poznan.pl>
2017-12-26 22:54:24 +0100

    first

---
+++ KryptografiaWizualna/MainWindow.xaml.cs
@@ -0,0 +1,174 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace KryptografiaWizualna
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private VisualCryptoBasic cryptoBasic = new VisualCryptoBasic();
+
+        public BitmapImage ConvertBitmap(System.Drawing.Bitmap bitmap)
+        {
+            MemoryStream ms = new MemoryStream();
+            bitmap.Save(ms, System.Drawing.Imaging.ImageFormat.Bmp);
+            BitmapImage image = new BitmapImage();
+            image.BeginInit();
+            ms.Seek(0, SeekOrigin.Begin);
+            image.StreamSource = ms;
+            image.EndInit();
+
+            return image;
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            TextBoxRedThreshold.Text = "127";
+            TextBoxGreenThreshold.Text = "127";
+            TextBoxBlueThreshold.Text = "127";
+        }
+
+        private void ButtonLoadImg_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == true)
+            {
+                cryptoBasic.LoadImage(openFileDialog.FileName);
+                ImageInput.Source = new BitmapImage(new Uri(openFileDialog.FileName));
+            }
+
+        }
+
+        private void ButtonEncryptBasic_Click(object sender, RoutedEventArgs e)
+        {
+            if (!int.TryParse(TextBoxRedThreshold.Text, out int R))
+            {
+                MessageBox.Show("Bład w polu threshold R");
+                return;
+            }
+                
+            if (!int.TryParse(TextBoxGreenThreshold.Text, out int G))
+            {
+                MessageBox.Show("Bład w polu threshold G");
+                return;
+            }
+                
+            if (!int.TryParse(TextBoxBlueThreshold.Text, out int B))
+            {
+                MessageBox.Show("Bład w polu threshold B");
+                return;
+            }
+
+
+            if (cryptoBasic.ImageLoaded)
+            {
+                try
+                {
+                    var result=cryptoBasic.Encrypt(R, G, B, (bool)CheckBoxTransparent.IsChecked);
+                    ImageComponent1.Source = ConvertBitmap(result[0]);
+                    ImageComponent2.Source = ConvertBitmap(result[1]);
+
+                    for (int i = 0; i < result.Length; i++)
+                    {
+                        result[i].Save(TextBoxComponents.Text + i+ComboBoxImageFormat.Text);
+                    }
+
+                    MessageBox.Show("Obrazy zapisane pomyślnie!");
+
+                    Process.Start(Directory.GetCurrentDirectory());
+
+
+            }catch(Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+                
+            }else
+            {
+                MessageBox.Show("Nie wczytano obrazu");
+            }
+        }
+
+        private void SliderRedColor_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            TextBoxRedThreshold.Text = SliderRedColor.Value.ToString();
+
+        }
+
+        private void TextBoxRedThreshold_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+            if (int.TryParse(TextBoxRedThreshold.Text, out int val))
+            {
+                if (val >= 0 && val <= 255)
+                {
+                    SliderRedColor.Value = val;
+                }
+            }
+            else
+            {
+                return;
+            }
+
+
+        }
+
+        private void SliderGreenColor_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            TextBoxGreenThreshold.Text = SliderGreenColor.Value.ToString();
+        }
+
+        private void TextBoxGreenThreshold_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (int.TryParse(TextBoxGreenThreshold.Text, out int val))
+            {
+                if (val >= 0 && val <= 255)
+                {
+                    SliderGreenColor.Value = val;
+                }
+            }
+            else
+            {
+                return;
+            }
+        }
+
+        private void SliderBlueColor_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            TextBoxBlueThreshold.Text = SliderBlueColor.Value.ToString();
+        }
+
+        private void TextBoxBlueThreshold_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (int.TryParse(TextBoxBlueThreshold.Text, out int val))
+            {
+                if (val >= 0 && val <= 255)
+                {
+                    SliderBlueColor.Value = val;
+                }
+            }
+            else
+            {
+                return;
+            }
+        }
+    }
+}
---
+++ KryptografiaWizualna/VisualCryptoBasic.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KryptografiaWizualna
+{
+    class VisualCryptoBasic
+    {
+        public static int[,] pixelsMatrix1 = new int[,] { { 0,1,0,1 },{ 1,0,1,0 },{0,0,1,1 },
+                                                    {1,1,0,0 },{0,1,1,0 },{1,0,0,1 } };
+
+        public static int[,] pixelsMatrix2 = new int[,] { { 1,0,1,0 },{ 0,1,0,1 },{1,1,0,0 },
+                                                    {0,0,1,1 },{1,0,0,1 },{0,1,1,0 } };
+
+
+        private Bitmap bmp;
+        public bool ImageLoaded = false;
+
+        private Color MapToColor(int value, bool whiteAsTransparent = false)
+        {
+            if (value == 0)
+            {
+                return Color.Black;
+            }
+            else
+            {
+                if (whiteAsTransparent)
+                    return Color.Transparent;
+                else
+                    return Color.White;
+            }
+        }
+
+        public Bitmap AssembleComponents(Bitmap[] components, bool removeNoise = false)
+        {
+            if (components.Length != 2)
+            {
+                throw new ArgumentException("Liczba udziałów musi być równa 2!");
+            }
+
+            if (components[0].Size != components[1].Size)
+            {
+                throw new ArgumentException("Obrazy muszą być tej samej wielkości!");
+            }
+
+
+            int width = components[0].Width;
+            int height = components[0].Height;
+            Console.WriteLine(width + " " + height);
+
+            Bitmap result = new Bitmap(width, height);
+
+            for (int i = 0; i < height; i += 2)
+            {
+                for (int j = 0; j < width; j += 2)
+                {
+                    bool pixelBlockIsEqual = true;
+
+                    Color[] c1 = new Color[4];
+                    c1[0] = components[0].GetPixel(j, i);
+                    c1[1] = components[0].GetPixel(j + 1, i);
+                    c1[2] = components[0].GetPixel(j, i + 1);
+                    c1[3] = components[0].GetPixel(j + 1, i + 1);
+
+                    Color[] c2 = new Color[4];
+                    c2[0] = components[1].GetPixel(j, i);
+                    c2[1] = components[1].GetPixel(j + 1, i);
+                    c2[2] = components[1].GetPixel(j, i + 1);
+                    c2[3] = components[1].GetPixel(j + 1, i + 1);
+
+                    for (int k = 0; k < c1.Length; k++)
+                    {
+                        if (c1[k] != c2[k])
+                        {
+                            pixelBlockIsEqual = false;
+                        }
+                    }
+
+                    if (pixelBlockIsEqual)
+                    {
+                        if (!removeNoise)
+                        {
+                            result.SetPixel(j, i, c1[0]);
+                            result.SetPixel(j + 1, i, c1[1]);
+                            result.SetPixel(j, i + 1, c1[2]);
+                            result.SetPixel(j + 1, i + 1, c1[3]);
+                        }
+                        else
+                        {
+                            result.SetPixel(j, i, Color.White);
+                            result.SetPixel(j + 1, i, Color.White);
+                            result.SetPixel(j, i + 1, Color.White);
+                            result.SetPixel(j + 1, i + 1, Color.White);
+                        }
+
+                    }
+                    else
+                    {
+                        result.SetPixel(j, i, Color.Black);
+                        result.SetPixel(j + 1, i, Color.Black);
+                        result.SetPixel(j, i + 1, Color.Black);
+                        result.SetPixel(j + 1, i + 1, Color.Black);
+                    }
+                }
+            }
+
+            return result;
+        }
+
+
+        public Bitmap[] Encrypt(int thresholdR = 127, int thresholdG = 127, int thresholdB = 127, bool whiteAsTransparent = false)
+        {
+            if (thresholdR < 0 || thresholdR > 255)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            if (thresholdG < 0 || thresholdG > 255)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            if (thresholdB < 0 || thresholdB > 255)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+
+            if (bmp == null)
+            {
+                throw new NullReferenceException("Nie wczytano obrazu!");
+            }
+
+            int width = bmp.Width;
+            int height = bmp.Height;
+
+            Bitmap[] components = new Bitmap[] { new Bitmap(width * 2, height * 2), new Bitmap(width * 2, height * 2) };
+
+            Random rng = new Random();
+
+            for (int i = 0; i < height; i++)
+            {
+                for (int j = 0; j < width; j++)
+                {
+                    Color c = bmp.GetPixel(j, i);
+
+                    if (c.R > thresholdR && c.G > thresholdG && c.B > thresholdB)
+                    {
+                        int randomIndex = rng.Next(0, 6);
+                        components[0].SetPixel(j * 2, i * 2, MapToColor(pixelsMatrix1[randomIndex, 0], whiteAsTransparent));
+                        components[0].SetPixel(j * 2 + 1, i * 2, MapToColor(pixelsMatrix1[randomIndex, 1], whiteAsTransparent));
+                        components[0].SetPixel(j * 2, i * 2 + 1, MapToColor(pixelsMatrix1[randomIndex, 2], whiteAsTransparent));
+                        components[0].SetPixel(j * 2 + 1, i * 2 + 1, MapToColor(pixelsMatrix1[randomIndex, 3], whiteAsTransparent));
+
+                        components[1].SetPixel(j * 2, i * 2, MapToColor(pixelsMatrix1[randomIndex, 0], whiteAsTransparent));
+                        components[1].SetPixel(j * 2 + 1, i * 2, MapToColor(pixelsMatrix1[randomIndex, 1], whiteAsTransparent));
+                        components[1].SetPixel(j * 2, i * 2 + 1, MapToColor(pixelsMatrix1[randomIndex, 2], whiteAsTransparent));
+                        components[1].SetPixel(j * 2 + 1, i * 2 + 1, MapToColor(pixelsMatrix1[randomIndex, 3], whiteAsTransparent));
+
+                    }
+                    else
+                    {
+                        int randomIndex = rng.Next(0, 6);
+                        components[0].SetPixel(j * 2, i * 2, MapToColor(pixelsMatrix1[randomIndex, 0], whiteAsTransparent));
+                        components[0].SetPixel(j * 2 + 1, i * 2, MapToColor(pixelsMatrix1[randomIndex, 1], whiteAsTransparent));
+                        components[0].SetPixel(j * 2, i * 2 + 1, MapToColor(pixelsMatrix1[randomIndex, 2], whiteAsTransparent));
+                        components[0].SetPixel(j * 2 + 1, i * 2 + 1, MapToColor(pixelsMatrix1[randomIndex, 3], whiteAsTransparent));
+
+                        components[1].SetPixel(j * 2, i * 2, MapToColor(pixelsMatrix2[randomIndex, 0], whiteAsTransparent));
+                        components[1].SetPixel(j * 2 + 1, i * 2, MapToColor(pixelsMatrix2[randomIndex, 1], whiteAsTransparent));
+                        components[1].SetPixel(j * 2, i * 2 + 1, MapToColor(pixelsMatrix2[randomIndex, 2], whiteAsTransparent));
+                        components[1].SetPixel(j * 2 + 1, i * 2 + 1, MapToColor(pixelsMatrix2[randomIndex, 3], whiteAsTransparent));
+
+                    }
+                }
+            }
+
+
+            return components;
+
+        }
+
+
+
+
+        public void LoadImage(string path)
+        {
+            bmp = new Bitmap(Image.FromFile(path));
+            ImageLoaded = true;
+        }
+
+
+    }
+}
